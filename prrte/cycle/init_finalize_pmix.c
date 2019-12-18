7c8b450b4af7efe704d41d7095109aec9a9cc9aa
Joshua Hursey <jhursey@us.ibm.com>
2019-12-18 10:19:59 -0600

    Setup PRRTE CI
     * `hello_world` test which runs `hostname` and a Hello World PMIx client
     * `cycle` which tests running a bunch of jobs against the same
       PRRTE server set including a single node `hostname` and an
       init/finalize PMIx application across all slots.
    
    Signed-off-by: Joshua Hursey <jhursey@us.ibm.com>

---
+++ prrte/cycle/init_finalize_pmix.c
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2019      IBM, Inc.  All rights reserved.
+ * $COPYRIGHT$
+ *
+ * Additional copyrights may follow
+ *
+ * $HEADER$
+ *
+ */
+
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+
+#include <pmix.h>
+
+
+int main(int argc, char **argv)
+{
+    pmix_status_t rc;
+    pid_t pid;
+    char hostname[1024];
+    pmix_proc_t myproc;
+
+    pid = getpid();
+    gethostname(hostname, 1024);
+
+    if( PMIX_SUCCESS != (rc = PMIx_Init(&myproc, NULL, 0)) ) {
+        fprintf(stderr, "ERROR: PMIx_Init failed (%d on %s): %d\n", (unsigned long)pid, hostname, rc);
+        exit(1);
+    }
+
+    if (PMIX_SUCCESS != (rc = PMIx_Finalize(NULL, 0))) {
+        fprintf(stderr, "ERROR: PMIx_Finalize failed (%d on %s): %d (%s)\n", (unsigned long)pid, hostname, rc, PMIx_Error_string(rc));
+        exit(1);
+    }
+
+    fflush(stderr);
+    return(0);
+}
---
+++ prrte/hello_world/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -e
+
+echo "=========================="
+echo "Building PMIx Hello World"
+echo "=========================="
+
+pcc hello.c -o hello
+
+
