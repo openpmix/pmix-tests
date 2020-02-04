fc7916e6b80ad8843d33bf66d9d68e37e0efa7d3
Joshua Hursey <jhursey@us.ibm.com>
2020-02-04 14:37:10 -0600

    Another test
    Signed-off-by: Joshua Hursey <jhursey@us.ibm.com>

--- prrte/cycle/build.sh
+++ prrte/cycle/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 
 echo "=========================="
-echo "Building PMIx Hello World"
+echo "Building PMIx Init/Finalize"
 echo "=========================="
 
 pcc init_finalize_pmix.c -o init_finalize_pmix
