d2adaa2ebf26520c41b25f15a56fdb8ac9fbc76e
Joshua Hursey <jhursey@us.ibm.com>
2020-05-11 14:34:33 -0500

    Move towards using pmixcc instead of pcc
     * Check for both just in case
    
    Signed-off-by: Joshua Hursey <jhursey@us.ibm.com>

--- prrte/cycle/build.sh
+++ prrte/cycle/build.sh
@@ -1,9 +1,23 @@
 #!/bin/bash -e
 
 echo "=========================="
+echo "Detect wrapper compiler"
+echo "=========================="
+set +e
+PCC=`which pmixcc`
+if [ $? != 0 ] ; then
+    PCC=`which pcc`
+    if [ $? != 0 ] ; then
+        echo "ERROR: Failed to find a wrapper compiler"
+        exit 1
+    fi
+fi
+echo "Compiler: $PCC"
+echo ${PCC} --showme
+set -e
+
+echo "=========================="
 echo "Building PMIx Hello World"
 echo "=========================="
 
-pcc init_finalize_pmix.c -o init_finalize_pmix
-
-
+${PCC} init_finalize_pmix.c -o init_finalize_pmix
