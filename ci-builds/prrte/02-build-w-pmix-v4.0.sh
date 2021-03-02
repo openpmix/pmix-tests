15fa16ac30e4f08afc45677fd3e974fefbe2eea4
Joshua Hursey <jhursey@us.ibm.com>
2021-03-01 21:40:30 -0500

    Add some 'special' builds for CI testing
    Signed-off-by: Joshua Hursey <jhursey@us.ibm.com>

---
+++ ci-builds/prrte/02-build-w-pmix-v4.0.sh
@@ -0,0 +1,99 @@
+#!/bin/bash -ex
+
+#
+# PRRTE requires a minimum of OpenPMIx v4.0.x
+#
+
+#--------------------------------
+# Sanity Checks
+#--------------------------------
+# Ignore $_PMIX_CHECKOUT as we will use the current v4.0.x branch
+
+if [ -z "${_PRRTE_CHECKOUT}" ]; then
+    echo "Error: No PRRTE checkout"
+    # git clone https://github.com/openpmix/prrte.git
+    exit 1
+fi
+
+if [ ! -d ${_PRRTE_CHECKOUT} ] ; then
+    echo "Error: No PRRTE checkout in ${_PRRTE_CHECKOUT}"
+    echo "Set the _PRRTE_CHECKOUT envar to your prrte checkout"
+    exit 1
+fi
+
+#--------------------------------
+# Setup a clean build location
+#--------------------------------
+if [ -z "${_BUILD_DIR}" ]; then
+    _BUILD_DIR=`mktemp -d $HOME/tmp-build-XXXXX`
+fi
+
+cd $_BUILD_DIR
+
+#--------------------------------
+# PMIx Build
+#--------------------------------
+echo "MY PMIX BUILD"
+pwd
+git clone -b v4.0 https://github.com/openpmix/openpmix.git
+cd `basename ${_PMIX_CHECKOUT}`
+
+#--------------------------------
+# Autogen
+#--------------------------------
+export AUTOMAKE_JOBS=20
+./autogen.pl
+
+#--------------------------------
+# Configure
+#--------------------------------
+./configure --prefix=${_BUILD_DIR}/install-pmix \
+            --disable-debug \
+            --with-libevent=${LIBEVENT_INSTALL_PATH} \
+            --with-hwloc=${HWLOC1_INSTALL_PATH}
+
+#--------------------------------
+# Make
+#--------------------------------
+make -j 10
+
+#--------------------------------
+# Make Install
+#--------------------------------
+make -j 10 install
+
+
+#--------------------------------
+# PRRTE Build
+#--------------------------------
+cd $_BUILD_DIR
+cp -R ${_PRRTE_CHECKOUT} .
+cd `basename ${_PRRTE_CHECKOUT}`
+
+#--------------------------------
+# Autogen
+#--------------------------------
+export AUTOMAKE_JOBS=20
+./autogen.pl
+
+#--------------------------------
+# Configure
+#--------------------------------
+./configure --prefix=${_BUILD_DIR}/install-prrte \
+            --disable-debug \
+            --with-pmix=${_BUILD_DIR}/install-pmix \
+            --with-libevent=${LIBEVENT_INSTALL_PATH} \
+            --with-hwloc=${HWLOC1_INSTALL_PATH}
+
+#--------------------------------
+# Make
+#--------------------------------
+make -j 10
+
+#--------------------------------
+# Make Install
+#--------------------------------
+make -j 10 install
+
+
+exit 0
