f8db9c8e51777c398b0313972982e227a4e099ed
Joshua Hursey <jhursey@us.ibm.com>
2021-07-21 17:22:19 -0500

    MPIR Shim Test: Fix bash test
    Signed-off-by: Joshua Hursey <jhursey@us.ibm.com>

--- prrte/mpir-shim/build.sh
+++ prrte/mpir-shim/build.sh
@@ -10,7 +10,7 @@ fi
 echo "=========================="
 echo "Checkout the current MPIR Shim"
 echo "=========================="
-if [ -f "mpir-to-pmix-guide" ] ; then
+if [ -e "mpir-to-pmix-guide" ] ; then
     rm -rf mpir-to-pmix-guide
 fi
 git clone https://github.com/openpmix/mpir-to-pmix-guide.git
