2f2ab5e769e19d52ebe631a650e4e5ba4ae4279d
Austen Lauria <awlauria@us.ibm.com>
2022-03-17 09:53:57 -0400

    prrte/dmodex: Fortify this test a little bit.
    - ppn 5 -> ppn 10
    - Add run for prterun
    - Check for a lost connection error.
    - Check prterun/prun return codes.
    
    Signed-off-by: Austen Lauria <awlauria@us.ibm.com>

--- prrte/dmodex/run.sh
+++ prrte/dmodex/run.sh
@@ -17,6 +17,34 @@ _shutdown()
     exit $FINAL_RTN
 }
 
+
+# ---------------------------------------
+# Run the test - Dmodex (PMIx)
+# ---------------------------------------
+prterun --hostfile $CI_HOSTFILE --map-by ppr:10:node ./dmodex 2>&1 | tee output.txt
+if [ $? -ne 0 ]; then
+    echo "Error - seems prterun failed."
+    exit 42
+fi
+
+# ---------------------------------------
+# Verify the results
+# ---------------------------------------
+ERRORS=`grep -i "error\|fail\|lost" output.txt | wc -l`
+if [[ $ERRORS -ne 0 ]] ; then
+    echo "ERROR: Error string detected in the output"
+    exit 1
+fi
+
+LINES=`grep -i "PMIx_Finalize successfully completed" output.txt | wc -l`
+if [[ $LINES -ne $(( 10 * $NUM_NODES )) ]] ; then
+    echo "ERROR: Incorrect number of lines of output"
+    exit 2
+fi
+
+rm output.txt
+
+# Now verify prun
 # ---------------------------------------
 # Start the DVM
 # ---------------------------------------
@@ -31,29 +59,29 @@ date
 sleep 5
 date
 
-
-# ---------------------------------------
-# Run the test - Dmodex (PMIx)
-# ---------------------------------------
-prun --map-by ppr:5:node ./dmodex 2>&1 | tee output.txt
+prun --map-by ppr:10:node ./dmodex 2>&1 | tee output.txt
+if [ $? -ne 0 ]; then
+    echo "Error - seems prun failed."
+    FINAL_RTN=43
+    _shutdown
+fi
 
 # ---------------------------------------
 # Verify the results
 # ---------------------------------------
-ERRORS=`grep -i "error\|fail" output.txt | wc -l`
+ERRORS=`grep -i "error\|fail\|lost" output.txt | wc -l`
 if [[ $ERRORS -ne 0 ]] ; then
     echo "ERROR: Error string detected in the output"
-    FINAL_RTN=1
+    FINAL_RTN=44
     _shutdown
 fi
 
 LINES=`grep -i "PMIx_Finalize successfully completed" output.txt | wc -l`
-if [[ $LINES -ne $(( 5 * $NUM_NODES )) ]] ; then
+if [[ $LINES -ne $(( 10 * $NUM_NODES )) ]] ; then
     echo "ERROR: Incorrect number of lines of output"
-    FINAL_RTN=2
+    FINAL_RTN=3
     _shutdown
 fi
-
 
 if [ $FINAL_RTN == 0 ] ; then
     echo "Success - dmodex"
