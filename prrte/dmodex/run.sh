267a02931405913bc79fc983f138fb546c429b11
Ralph Castain <rhc@pmix.org>
2022-07-18 08:53:35 -0700

    Re-enable the dmodex test
    Ensure prun is pointed at the correct prte
    
    Signed-off-by: Ralph Castain <rhc@pmix.org>

--- prrte/dmodex/run.sh
+++ prrte/dmodex/run.sh
@@ -2,6 +2,7 @@
 
 # Final return value
 FINAL_RTN=0
+PRTEPID=0
 
 # Number of nodes - for accounting/verification purposes
 # Default: 1
@@ -12,7 +13,11 @@ _shutdown()
     # ---------------------------------------
     # Cleanup DVM
     # ---------------------------------------
+if [ $PRTEPID == 0 ] ; then
     pterm
+else
+    pterm --pid $PRTEPID
+fi
     cat $CI_HOSTFILE | xargs -L1 -I '{}'  ssh {} rm -rf /tmp/prte.*
     exit $FINAL_RTN
 }
@@ -21,8 +26,10 @@ cat $CI_HOSTFILE | xargs -L1 -I '{}'  ssh {} rm -rf /tmp/prte.*
 
 # ---------------------------------------
 # Run the test - Dmodex (PMIx)
+# Leave session attached in case one of the daemons
+# reports an error
 # ---------------------------------------
-prterun --hostfile $CI_HOSTFILE --map-by ppr:10:node ./dmodex 2>&1 | tee output.txt
+prterun --hostfile $CI_HOSTFILE --map-by ppr:10:node --leave-session-attached ./dmodex 2>&1 | tee output.txt
 if [ $? -ne 0 ]; then
     echo "Error - seems prterun failed."
     exit 42
@@ -45,7 +52,9 @@ fi
 
 rm output.txt
 
-# Now verify prun
+# Now verify prun - clear any cruft first
+cat $CI_HOSTFILE | xargs -L1 -I '{}'  ssh {} rm -rf /tmp/prte.*
+
 # ---------------------------------------
 # Start the DVM
 # ---------------------------------------
@@ -54,13 +63,14 @@ if [ "x" = "x$CI_HOSTFILE" ] ; then
 else
     prte --no-ready-msg --hostfile $CI_HOSTFILE &
 fi
+PRTEPID=$!
 
 date
 # Wait for DVM to start
 sleep 5
 date
 
-prun --map-by ppr:10:node ./dmodex 2>&1 | tee output.txt
+prun --map-by ppr:10:node --pid $PRTEPID ./dmodex 2>&1 | tee output.txt
 if [ $? -ne 0 ]; then
     echo "Error - seems prun failed."
     FINAL_RTN=43
