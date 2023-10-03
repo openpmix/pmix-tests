644c9a927a67bccc46f92f3f99042ffbcf10185d
Ralph Castain <rhc@pmix.org>
2023-10-03 10:29:36 -0600

    Setup to manually trigger a rebuild
    Signed-off-by: Ralph Castain <rhc@pmix.org>

--- crossversion/support/bin/check-for-updates.py
+++ crossversion/support/bin/check-for-updates.py
@@ -12,7 +12,7 @@ import subprocess
 import shutil
 
 # put this in one place
-supported_versions = ["master", "v4.1", "v4.0", "v3.2", "v3.1", "v3.0", "v2.2", "v2.1", "v2.0", "v1.2"]
+supported_versions = ["master", "v5.0", "v4.1", "v4.0"]
 
 pmix_git_url      = "https://github.com/pmix/pmix.git"
 pmix_release_url  = "https://github.com/pmix/pmix/releases/download/"
